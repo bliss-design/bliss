b4b3e53c8d447d938453d7bce9362e15c01716bc
Joe Takara <44985154+joetakara@users.noreply.github.com>
2023-06-24 21:33:22 +0700

    feat(utils): Implement withAsPolymorphic HOC (#12)

---
+++ packages/with-as-polymorphic/README.md
@@ -0,0 +1,33 @@
+# @bliss/with-as-polymorphic
+
+**This utility is intended for internal use only and should not be used publicly.**
+
+## Installation
+
+To install this package, you can use npm, yarn or pnpm:
+
+npm:
+
+```sh
+npm install @bliss/with-as-polymorphic
+```
+
+yarn:
+
+```sh
+yarn add @bliss/with-as-polymorphic
+```
+
+pnpm:
+
+```sh
+pnpm install @bliss/with-as-polymorphic
+```
+
+## Contribution
+
+For information on contributing to the project, please see the [contributing guidelines](https://github.com/bliss-design/bliss/blob/main/CONTRIBUTING.md).
+
+## License
+
+This project is licensed under the MIT License. Please refer to the [LICENSE](https://github.com/bliss-design/bliss/blob/main/LICENSE) file for more information.
---
+++ packages/with-as-polymorphic/src/with-as-polymorphic.types.ts
@@ -0,0 +1,38 @@
+import React from 'react';
+
+type MergeProps<ExistingProps = unknown, NewProps = unknown> = NewProps &
+  Omit<ExistingProps, keyof NewProps>;
+
+type JSXElement =
+  | keyof React.JSX.IntrinsicElements
+  | React.JSXElementConstructor<unknown>;
+
+type ManagedAttributes<Element extends JSXElement> =
+  React.JSX.LibraryManagedAttributes<
+    Element,
+    React.ComponentPropsWithoutRef<Element>
+  >;
+
+type ElementProp<Element> = {
+  as?: Element;
+};
+
+type MergedProps<
+  Element extends JSXElement,
+  AdditionalProps = unknown,
+> = MergeProps<ManagedAttributes<Element>, AdditionalProps>;
+
+type ElementRef<Element> = Element extends React.ElementType
+  ? React.ComponentPropsWithRef<Element>['ref']
+  : never;
+
+export type ComponentPropsWithAs<
+  Element,
+  AdditionalProps = Record<string, never>,
+> = Element extends React.ElementType
+  ? MergedProps<Element, AdditionalProps & ElementProp<Element>> & {
+      ref?: ElementRef<Element>;
+    }
+  : AdditionalProps & {
+      as: React.ElementType;
+    };
